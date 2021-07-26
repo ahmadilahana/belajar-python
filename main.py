11ad81fbec3c6ca7b3bfbc8ee49d53cbbdea8905
ahmadilahana <ahmadilahana@gmailcom>
2021-07-26 19:09:57 +0700

    intro

---
+++ main.py
@@ -0,0 +1,20 @@
+#import libary time
+import time
+start_time = time.time()
+
+print('heloo world')
+print('apa kabar')
+
+# comment
+
+#variable
+a = 10
+print(a)
+
+""" paragraf comment
+
+this paragraf commment """
+
+# use python -m py_compile main.py for compile code python to bytecode
+
+print(time.time() - start_time, 'detik')
